d5e55c4f8895fa3a53dc6cd0e3cdfb4d8eef8200
wesgriffin3 <52769190+wesgriffin3@users.noreply.github.com>
2019-07-11 13:59:57 -0500

    Update 20190711152534_create_quotes.rb

--- db/migrate/20190711152534_create_quotes.rb
+++ db/migrate/20190711152534_create_quotes.rb
@@ -1,6 +1,6 @@
 class CreateQuotes < ActiveRecord::Migration[5.0]
   def change
-    create_table :quote do |t|
+    create_table :quotes do |t|
       t.string :saying
       t.string :author
       t.timestamps
